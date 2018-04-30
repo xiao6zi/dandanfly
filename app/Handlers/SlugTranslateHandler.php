84b26a7557c8c2688b77048b9e5f375bcc301f2d
xiao6zi <540015488@qq.com>
2018-04-30 17:34:56 +0000

    增加 Translate

---
+++ app/Handlers/SlugTranslateHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Handlers;
+
+use GuzzleHttp\Client;
+
+class SlugTranslateHandler
+{
+    public function translate($text)
+    {
+    	$client = new Client();
+    	$api_url = "api.baidu";
+    	$appid = "";
+    	$app
+    }
+
+    public function pinying($text)
+    {
+
+    }
+
+}
