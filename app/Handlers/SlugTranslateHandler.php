786ef397bd4e9fdb1694ae2f9f0fa9d54099e53c
xiao6zi <540015488@qq.com>
2018-05-01 15:46:11 +0000

    加入百度翻译

--- app/Handlers/SlugTranslateHandler.php
+++ app/Handlers/SlugTranslateHandler.php
@@ -9,14 +9,43 @@ class SlugTranslateHandler
     public function translate($text)
     {
     	$client = new Client();
-    	$api_url = "api.baidu";
-    	$appid = "";
-    	$app
+    	$api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
+        $appid = config('services.baidu_translate.appid');
+        $key = config('services.baidu_translate.key');
+        $salt = time();
+
+        if (empty($appid) || empty($appid)) {
+            $this->pinying($text);
+
+        }
+
+        $sign = md5($appid . $text . $salt . $key);
+
+        $params = http_build_query([
+            "q"     =>  $text,
+            "from"  => "zh",
+            "to"    => "en",
+            "appid" => $appid,
+            "salt"  => $salt,
+            "sign"  => $sign,
+        ]);
+
+        $respone = $client->get($api, $params);
+
+        $result = json_decode($respone, true);
+
+        // 尝试获取获取翻译结果
+        if (isset($result['trans_result'][0]['dst'])) {
+            return str_slug($result['trans_result'][0]['dst']);
+        } else {
+            // 如果百度翻译没有结果，使用拼音作为后备计划。
+            return $this->pinyin($text);
+        }
     }
 
     public function pinying($text)
     {
-
+        return str_slug($text);
     }
 
 }
