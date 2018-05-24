1944e906dd1b30c740c312baea7882b5018fbd12
xiao6zi <540015488@qq.com>
2018-05-24 15:51:54 +0800

    更换simplemde编辑器

--- resources/views/articles/create_and_edit.blade.php
+++ resources/views/articles/create_and_edit.blade.php
@@ -41,7 +41,7 @@
                         </div>
 
                         <div class="form-group">
-                            <textarea name="body_original" class="form-control" id="editor" rows="15" placeholder="请使用 Markdown 格式书写 ;-)，代码片段黏贴时请注意使用高亮语法。" required>{{ old('body', trim($article->body_original) ) }}</textarea>
+                            <textarea name="body_original" class="form-control" id="editor" rows="15" placeholder="请使用 Markdown 格式书写 ;-)，代码片段黏贴时请注意使用高亮语法。" >{{ old('body', trim($article->body_original) ) }}</textarea>
                         </div>
 
                         <div class="well well-sm">
@@ -55,28 +55,15 @@
 @stop
 
 @section('styles')
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/simplemde.min.css') }}">
 @stop
 
 @section('scripts')
-    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simplemde.min.js') }}"></script>
 
     <script>
         $(document).ready(function(){
-            var editor = new Simditor({
-                textarea: $('#editor'),
-                {{--upload: {--}}
-                    {{--url: '{{ route('topics.upload_image') }}',--}}
-                    {{--params: { _token: '{{ csrf_token() }}' },--}}
-                    {{--fileKey: 'upload_file',--}}
-                    {{--connectionCount: 3,--}}
-                    {{--leaveConfirm: '文件上传中，关闭此页面将取消上传。'--}}
-                {{--},--}}
-//                pasteImage: true,
-            });
+          var simplemde = new SimpleMDE({ element: $("#editor")[0] });
         });
     </script>
 
