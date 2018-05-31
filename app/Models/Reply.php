e0527aea9647b262e63787c4a0ee7e31c053b938
xiao6zxi <540015488@qq.com>
2018-05-31 15:49:02 +0000

    user center replies

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('id', 'desc');
+    }
 }
