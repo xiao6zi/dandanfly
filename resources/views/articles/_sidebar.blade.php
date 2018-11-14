05c92d0a488511fc1945fc70c67fff3ae07e5c47
xiao6zi <540015488@qq.com>
2018-11-14 22:52:17 +0800

    add 今日诗词 api

--- resources/views/articles/_sidebar.blade.php
+++ resources/views/articles/_sidebar.blade.php
@@ -1,8 +1,26 @@
 <div class="panel panel-default">
     <div class="panel-body">
-        <a href="{{ route('articles.create') }}" class="btn btn-success btn-block" aria-label="Left Align">
-            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 新建帖子
-        </a>
+        <div class="text-center">今日诗词</div>
+        <hr>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <span class="panel-title">
+                    <a data-toggle="collapse" id="jinrishici" data-parent="#accordion" href="#collapseOne">
+                        正在加载今日诗词....
+                    </a>
+                </span>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse">
+                <div class="panel panel-default" style="margin-bottom: 0;">
+                    <div class="panel-heading">
+                        <h4 id="jinrishici-title">题目</h4>
+                        <span id="jinrishici-author" style="color:#636b6f;margin-left:5px;">作者：未知</span>
+                    </div>
+                    <div class="panel-body" id="jinrishici-body" style="font-size: 12px;"></div>
+                    <div class="panel-footer" id="jinrishici-footer"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
@@ -45,3 +63,33 @@
         </div>
     </div>
 @endif
+
+@section('scripts')
+    <script>
+		$.ajax({
+			url: 'https://v2.jinrishici.com/one.json',
+			xhrFields: {
+				withCredentials: true
+			},
+			success: function (result, status) {
+				console.log(result);
+				if (result.status === 'success') {
+					$('#jinrishici').html(result.data.content);
+					let contentHtml = '';
+					let footerHtml = '关键词：';
+					result.data.origin.content.forEach(function (val) {
+						contentHtml += '<p>' + val + '</p>';
+					})
+					footerHtml += result.data.matchTags.join(',');
+                    $('#jinrishici-author').html('作者：' + result.data.origin.author + '(' + result.data.origin.dynasty + ')')
+                    $('#jinrishici-title').html(result.data.origin.title);
+                    $('#jinrishici-body').html(contentHtml);
+                    $('#jinrishici-footer').html(footerHtml);
+                }
+			},
+            error: function () {
+                $('#jinrishici').html('今日诗词加载失败')
+			}
+		});
+    </script>
+@stop
