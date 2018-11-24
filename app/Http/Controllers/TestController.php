0ba1b488f1768bf36021e09678f7b31ef1cf8841
xiao6zi <540015488@qq.com>
2018-11-24 09:51:27 +0800

    add aliyun sms

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,7 +8,22 @@ class TestController extends Controller
 {
     public function test(User $user)
     {
-        $test =  $user->getActiveUsers();
-        dd($test);
+        $this->sms();
+    }
+
+    public function sms()
+    {
+        $sms = app('easysms');
+        try {
+            $sms->send(13117735073, [
+                'template' => 'SMS_151766554',
+                'data' => [
+                    'code' => 6379
+                ],
+            ]);
+        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+            $message = $exception->getException('aliyun')->getMessage();
+            dd($message);
+        }
     }
 }
