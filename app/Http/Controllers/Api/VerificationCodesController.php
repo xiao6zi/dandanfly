24ef03d6408021b5e563e614d7b2d9e66d906fe5
xiao6zi <540015488@qq.com>
2018-12-17 17:55:02 +0800

    user can register using phone

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -14,19 +14,23 @@ class VerificationCodesController extends Controller
 
         $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
-        try {
-            $easySms->send($phone, [
-                'template' => 'SMS_151766554',
-                'data' => [
-                    'code' => $code
-                ],
-            ]);
-        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-            $message = $exception->getException('aliyun')->getMessage();
-            return $this->response->errorInternal($message ?: '短信发送异常');
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            try {
+                $easySms->send($phone, [
+                    'template' => 'SMS_151766554',
+                    'data' => [
+                        'code' => $code
+                    ],
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('aliyun')->getMessage();
+                return $this->response->errorInternal($message ?: '短信发送异常');
+            }
         }
 
-        $key = 'verificationCode_'.str_random(15);
+        $key = 'verificationCode_' . str_random(15);
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar', 'introduction'
+        'name', 'phone', 'email', 'password', 'avatar', 'introduction'
     ];
 
     /**
