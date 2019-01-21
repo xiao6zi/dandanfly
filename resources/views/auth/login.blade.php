99489b0ef80d372787fdcbd3ee96fd8f4aaf67a5
xiao6zi <540015488@qq.com>
2019-01-21 18:09:12 +0800

    Interface optimization

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,50 +7,6 @@
                 <div class="panel panel-default col-md-6 col-md-offset-3">
                     <div class="panel-heading">用户登录</div>
                     <div class="panel-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email">邮箱:</label>
-                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password">密码:</label>
-                                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> 记住我
-                                </label>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary col-md-12">登录</button>
-                                <a class="btn btn-link text-muted forget-password" href="{{ route('password.request') }}">
-                                    忘记密码?
-                                </a>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    <a class="btn btn-link" href="{{ route('login.github') }}">
-                                        <i class="iconfont icon-github"></i> 使用Github登录
-                                    </a>
-                                </div>
-                                {{--<div class="col-xs-6 col-sm-6 col-md-6">--}}
-                                    {{--<a class="btn btn-link pull-right" href="{{ route('socials.authorizations.login', ['type' => 'wechat']) }}">--}}
-                                        {{--<i class="iconfont icon-wechat"></i> 使用微信登录--}}
-                                    {{--</a>--}}
-                                {{--</div>--}}
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            桂ICP备16004823号-3
         </p>
 
-        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:xiao6zi@xiao6zi.com">联系我们</a></p>
     </div>
 </footer>
