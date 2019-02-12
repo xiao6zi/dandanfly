0764227b1511cac3f54504591e65c7c51bcd1e59
xiao6zi <540015488@qq.com>
2019-02-12 11:17:18 +0800

    login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,11 @@
                 <div class="panel panel-default col-md-6 col-md-offset-3">
                     <div class="panel-heading">用户登录</div>
                     <div class="panel-body">
+                        <div class="form-group">
+                            <a class="btn btn-primary col-md-12" href="{{ route('login.github') }}">
+                                <i class="iconfont icon-github"></i> 使用Github登录
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
